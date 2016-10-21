52523fb3f2d6e3844cf4060844efe5fbe6ed587a
Hermes Espínola <hermesespinola@gmail.com>
2016-10-20 23:16:48 -0500

    fixed node classes

--- src/node/BinaryTreeNode.java
+++ src/node/BinaryTreeNode.java
@@ -1,6 +1,14 @@
-public class BinaryTreeNode<K, V> extends KeyValueNode<K, V> {
+package node;
+
+public class BinaryTreeNode<K extends Comparable<K>, V> extends KeyValueNode<K, V> {
   BinaryTreeNode<K, V> left;
   BinaryTreeNode<K, V> right;
+
+  public BinaryTreeNode(K key, V val, BinaryTreeNode<K, V> left, BinaryTreeNode<K, V> right) {
+    super(key, val);
+    this.left = left;
+    this.right = right;
+  }
 
   public BinaryTreeNode<K, V> left() {
     return this.left;
