df9bf65f8d1d0e8b4a966b04ab9d355d0d758f1f
Hermes Espínola <hermesespinola@gmail.com>
2016-11-11 17:35:26 -0600

    DisjointSet

---
+++ src/set/DisjointSet.java
@@ -0,0 +1,90 @@
+import java.util.Arrays;
+
+public class DisjointSet {
+  int[] p; // array of parents
+  int[] rank; // rank of each set
+  int[] size; // size of each set
+  int numSets; // number of sets
+
+  public DisjointSet(int nSets) {
+    this.makeSet(nSets);
+  }
+
+  /**
+  * initialize n sets
+  * @param n number of sets to initialize
+  * @return lo que regresa
+  */
+  public void makeSet(int n) {
+    p = new int[n];
+    for (int i = 0; i < n; i++) {
+      p[i] = i;
+    }
+    rank = new int[n];
+
+    size = new int[n];
+    for (int i = 0; i < n; i++) {
+      size[i] = 1;
+    }
+    numSets = n;
+  }
+
+  /**
+  * find the parent of the set x belongs to
+  */
+  public int find(int x) {
+    if (x < 0 || x >= numSets) {
+      throw new IndexOutOfBoundsException();
+    }
+    int i = x;
+    while (i != p[i]) i = p[i];
+    p[x] = i;
+    return i;
+  }
+
+  public boolean isSameSet(int i, int j) {
+    return find(i) == find(j);
+  }
+
+  public void union(int i, int j) {
+    int iParent = find(i);
+    int jParent = find(j);
+    if (iParent != jParent) {
+      if (rank[iParent] <= rank[jParent]) {
+        p[jParent] = iParent;
+        size[iParent] += size[jParent];
+        if (rank[iParent] == rank[jParent]) {
+          rank[iParent] += 1;
+        }
+      } else {
+        p[iParent] = jParent;
+        size[jParent] += size[iParent];
+        if (rank[iParent] == rank[jParent]) {
+          rank[jParent] += 1;
+        }
+      }
+      numSets--;
+    }
+  }
+
+  public int numDisjointSets() {
+    return numSets;
+  }
+
+  public int sizeOfSet(int i) {
+    return size[find(i)];
+  }
+
+  public String toString() {
+    return Arrays.toString(p);
+  }
+
+  public static void main(String[] args) {
+    DisjointSet ds = new DisjointSet(10);
+    ds.union(2, 6);
+    ds.union(1, 2);
+    System.out.println(ds);
+    System.out.println(ds.numDisjointSets());
+    System.out.println(ds.sizeOfSet(6));
+  }
+}
