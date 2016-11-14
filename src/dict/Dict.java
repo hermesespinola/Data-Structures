f7600bd78ca7a04067381b6e8763042bf9f58d4e
Hermes Espínola <hermesespinola@gmail.com>
2016-11-13 20:11:55 -0600

    dict todos

--- src/dict/Dict.java
+++ src/dict/Dict.java
@@ -3,13 +3,16 @@ package dict;
 import java.util.Iterator;
 
 public interface Dict<K,V> {
+  // TODO: change name to set
   public void add(K key, V value);
   public V remove(K key);
+  // TODO: change name to get
   public V getValue(K key);
   public boolean contains(K key);
   public Iterable<K> keys();
   public Iterable<V> values();
   public boolean isEmpty();
+  // TODO: change name to size
   public int getSize();
   public void clear();
 }
